766e97e9af48023e6661fa5fce4642325867feda
katherines arvizu <antiogitos2018@gmail.com>
2019-04-05 10:28:37 -0600

    iniciar el juego

--- js/index.js
+++ js/index.js
@@ -66,14 +66,22 @@ function draw() {
   //se llama la funcion de dibujar una paleta
   drawPaddle();
 
-//verificar si llego al limite de arriba/abajo.
-  if(y + dy < 0 || y + dy > canvas.height) {
-    dy = -dy;
+  //verificar si llego al limite de la izquierda/derecho.
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
   }
 
-  //verificar si llego al limite de la izquierda/derecho.
-  if(x + dx < 0 || x + dx > canvas.width) {
-    dx = -dx;
+
+  if (y + dy < ballRadius ) {
+    dy = -dy;
+  } else if (y + dy > canvas.height -ballRadius) {
+    if (x > paddleX && paddleX + paddleWidth) {
+    dy = -dy ;
+  } else  {
+   alert("Moriste");
+   document.location.reload();
+  }
+
   }
 
   //verificar si se toco la tecla direccional derecha
